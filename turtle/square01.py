9f72f0ee34413bd6636f332b3d4e02ab99554394
Victor Gau <victorgau@gmail.com>
2018-03-23 10:25:28 +0800

    update turtle files

--- turtle/square01.py
+++ turtle/square01.py
@@ -6,16 +6,16 @@ screen = turtle.Screen()
 screen.setup(400, 400)
 
 # 產生 turtle
-turtle = turtle.Turtle()
+t = turtle.Turtle()
 
 # 控制 turtle 行進的方向，以產生圖形
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
 
 # 當 screen 被滑鼠點擊時，離開程式
 screen.exitonclick()
--- turtle/square03.py
+++ turtle/square03.py
@@ -3,29 +3,29 @@ import turtle
 screen = turtle.Screen()
 screen.setup(400, 400)
 
-turtle = turtle.Turtle()
+t = turtle.Turtle()
 
-def square(x, y, length):
+def square(t, x, y, length):
     # x: x 座標
     # y: y 座標
     # length: 邊的長度
     
-    turtle.penup()
-    turtle.goto(x, y)           # 將 turtle 移到指定的位置
-    turtle.setheading(0)        # 將 turtle 朝向特定的角度
-    turtle.pendown()
+    t.penup()
+    t.goto(x, y)           # 將 turtle 移到指定的位置
+    t.setheading(0)        # 將 turtle 朝向特定的角度
+    t.pendown()
 
     # 畫一個正方形
-    turtle.forward(length)
-    turtle.left(90)
-    turtle.forward(length)
-    turtle.left(90)
-    turtle.forward(length)
-    turtle.left(90)
-    turtle.forward(length)
+    t.forward(length)
+    t.left(90)
+    t.forward(length)
+    t.left(90)
+    t.forward(length)
+    t.left(90)
+    t.forward(length)
 
-square(50, 50, 100)
-square(-50, -50, 50)
-square(-50, 50, 30)
+square(t, 50, 50, 100)
+square(t, -50, -50, 50)
+square(t, -50, 50, 30)
 
 screen.exitonclick()
